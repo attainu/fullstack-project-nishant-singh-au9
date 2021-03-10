e5a97972fa5a04b725b25949763101004367fb6d
Lawrence <lawrence1992.pr@gmail.com>
2021-03-10 11:10:47 +0530

    login and register page

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    //Enter MONGO DB URL here
+    "mongodb+srv://lawrence439:lawrence@cluster0.2v27s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "MYSECRET",
+};
